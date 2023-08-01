3a255a1f63e5d643b4e2565b98e597749da05037
anhquan2211 <anhquanst2211@gmail.com>
2023-08-02 06:50:36 +0700

    fix exercise 06

--- f8-offline-day17/main.js
+++ f8-offline-day17/main.js
@@ -138,7 +138,7 @@ for (let i = 0; i < 64; i++) {
   document
     .getElementById("mainChessBoard")
     .appendChild(document.createElement("div")).style.backgroundColor =
-    parseInt(i / 8 + i) % 2 == 0 ? "#ababab" : "white";
+    parseInt(i / 8 + i) % 2 === 0 ? "#ababab" : "white";
 }
 
 /*
