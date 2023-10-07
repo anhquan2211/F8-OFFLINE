9e8171a12744c3e81917fe51ba27a4e51d43a59c
Anh Quan <anhquan@QuanMacbookProM2.local>
2023-10-08 00:16:31 +0700

    Add noti viewed all posts for users

--- f8-offline-day35/assets/js/main.js
+++ f8-offline-day35/assets/js/main.js
@@ -6,6 +6,7 @@ const loading = document.querySelector(".loader");
 
 let limit = 3;
 let page = 1;
+let allDataLoaded = false;
 
 // Fetch the posts data from api
 async function getPosts() {
@@ -13,6 +14,13 @@ async function getPosts() {
     `https://ff9cn8-8080.csb.app/posts?_limit=${limit}&_page=${page}`
   );
   const data = await res.json();
+
+  console.log(data);
+
+  if (data.length === 0) {
+    console.log("allDataLoaded = true");
+    allDataLoaded = true;
+  }
 
   return data;
 }
@@ -36,7 +44,7 @@ async function showPosts() {
         <div class="desc-wrap">
           <div class="name">
             ${post.name}
-            
+
             <svg fill="currentColor" viewBox="0 0 12 13" width="1em" height="1em" class="x1lliihq x1k90msu x2h7rmj x1qfuztq x1qq9wsj x1kpxq89 xsmyaan" title="Tài khoản đã xác minh"><title>Tài khoản đã xác minh</title><g fill-rule="evenodd" transform="translate(-98 -917)"><path d="m106.853 922.354-3.5 3.5a.499.499 0 0 1-.706 0l-1.5-1.5a.5.5 0 1 1 .706-.708l1.147 1.147 3.147-3.147a.5.5 0 1 1 .706.708m3.078 2.295-.589-1.149.588-1.15a.633.633 0 0 0-.219-.82l-1.085-.7-.065-1.287a.627.627 0 0 0-.6-.603l-1.29-.066-.703-1.087a.636.636 0 0 0-.82-.217l-1.148.588-1.15-.588a.631.631 0 0 0-.82.22l-.701 1.085-1.289.065a.626.626 0 0 0-.6.6l-.066 1.29-1.088.702a.634.634 0 0 0-.216.82l.588 1.149-.588 1.15a.632.632 0 0 0 .219.819l1.085.701.065 1.286c.014.33.274.59.6.604l1.29.065.703 1.088c.177.27.53.362.82.216l1.148-.588 1.15.589a.629.629 0 0 0 .82-.22l.701-1.085 1.286-.064a.627.627 0 0 0 .604-.601l.065-1.29 1.088-.703a.633.633 0 0 0 .216-.819"></path></g></svg>  
           </div>
           <div class="info">
@@ -123,13 +131,18 @@ async function showPosts() {
 function showLoading() {
   loading.classList.add("show");
 
-  setTimeout(() => {
+  setTimeout(async () => {
     loading.classList.remove("show");
 
-    setTimeout(() => {
-      page++;
-      showPosts();
-    }, 300);
+    page++;
+    await showPosts();
+
+    if (allDataLoaded) {
+      const message = document.createElement("div");
+      message.textContent = "You have viewed all the posts";
+      message.classList.add("end-message");
+      postsContainer.appendChild(message);
+    }
   }, 1000);
 }
 
@@ -140,7 +153,7 @@ showPosts();
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
+  if (!allDataLoaded && scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
