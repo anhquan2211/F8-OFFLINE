66e2c57abe4d1bb68d5cf50c6cc3f239a7f99095
anhquan2211 <anhquanst2211@gmail.com>
2023-11-19 02:18:29 +0700

    fix page login

--- f8-offline-day47/src/pages/Auth/Login/Login.jsx
+++ f8-offline-day47/src/pages/Auth/Login/Login.jsx
@@ -47,16 +47,46 @@ function Login({ onLoginSuccess }) {
   return (
     <div className="login-container">
       <div className="overlay"></div>
-      <div className="login-content">
-        <h1 className="title">Đăng nhập</h1>
-        <p className="description">Vui lòng nhập địa chỉ email đã đăng ký F8</p>
-        <form className="login-form" onSubmit={handleSubmitLogin}>
-          <input type="text" placeholder="Email của bạn..." name="email" />
 
-          <button className="btn-login" type="submit">
-            Login
-          </button>
-        </form>
+      <div className="login-content">
+        <img
+          alt="Trello"
+          className="trello-main-logo"
+          src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/167dc7b9900a5b241b15ba21f8037cf8/trello-logo-blue.svg"
+        ></img>
+        <div className="content-container">
+          <h1 className="title">Đăng nhập vào Trello</h1>
+          <p className="description">
+            Vui lòng nhập địa chỉ email đã đăng ký F8
+          </p>
+          <form className="login-form" onSubmit={handleSubmitLogin}>
+            <input type="text" placeholder="Email của bạn..." name="email" />
+
+            <button className="btn-login" type="submit">
+              Login
+            </button>
+          </form>
+          <span className="or">HOẶC</span>
+          <a href="#!" className="btn ">
+            <span className="icon icon-google"></span>
+            <span className="label">Tiếp tục với Google</span>
+          </a>
+
+          <a href="#!" className="btn ">
+            <span className="icon icon-microsoft"></span>
+            <span className="label">Tiếp tục với Microsoft</span>
+          </a>
+
+          <a href="#!" className="btn ">
+            <span className="icon icon-apple"></span>
+            <span className="label">Tiếp tục với Apple</span>
+          </a>
+
+          <a href="#!" className="btn ">
+            <span className="icon icon-slack"></span>
+            <span className="label">Tiếp tục với Slack</span>
+          </a>
+        </div>
       </div>
       {loading && <Loading />}
       {/* <ToastContainer /> */}
